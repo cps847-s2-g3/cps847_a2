3ca23fb1951c8e5438dfb5fb003342aea8f2e64f
Wayne Tong <wayne.tong@ryerson.ca>
2018-02-14 18:28:36 -0500

    Add quick_maths library

---
+++ quick_maths.py
@@ -0,0 +1,5 @@
+def two_plus_two():
+    return 4
+
+def four_minus_one():
+    return 3
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,7 +1,8 @@
 import pytest
+from quick_maths import two_plus_two, four_minus_one
 
 def test_2_plus_2():
-    assert(2+2 == 4)
+    assert(two_plus_two() == 4)
 
 def test_4_minus_1():
-    assert(4-1 == 3)
+    assert(four_minus_one() == 3)
